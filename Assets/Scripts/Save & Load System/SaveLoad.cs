538f97ce859adb524765de7e50aac64283d05a2d
SinanEmiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2020-08-28 23:38:42 +0300

    Created 'SaveLoad' & Surrogates for Vector3 & Quaternion

---
+++ Assets/Scripts/Save & Load System/SaveLoad.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public sealed class SaveLoad
+{
+    static readonly string _savePath = Application.persistentDataPath + "/Saves/";
+
+    public static void Save<T>(T objToSave, string key)
+    {
+        BinaryFormatter formatter = GetBinaryFormatter();
+
+        if (!Directory.Exists(_savePath))
+            Directory.CreateDirectory(_savePath);
+
+        string path = $"{_savePath}{key}.txt";
+
+        using (FileStream fileStream = new FileStream(path, FileMode.Create))
+            formatter.Serialize(fileStream, objToSave);
+    }
+
+    public static T Load<T>(string key)
+    {
+        T returnValue = default;
+
+        BinaryFormatter formatter = GetBinaryFormatter();
+        string path = $"{_savePath}{key}.txt";
+
+        using (FileStream fileStream = new FileStream(path, FileMode.Open))
+            returnValue = (T)formatter.Deserialize(fileStream);
+
+        return returnValue;
+    }
+
+    public static bool SaveExists(string key)
+    {
+        string path = $"{_savePath}{key}.txt";
+        return File.Exists(path);
+    }
+
+    private static BinaryFormatter GetBinaryFormatter()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        SurrogateSelector selector = new SurrogateSelector();
+
+        Vector3SerializationSurrgote vector3Surrgote = new Vector3SerializationSurrgote();
+        QuaternionSerializationSurrogate quaternionSurrogate = new QuaternionSerializationSurrogate();
+
+        selector.AddSurrogate(typeof(Vector3), new StreamingContext(StreamingContextStates.All), vector3Surrgote);
+        selector.AddSurrogate(typeof(Quaternion), new StreamingContext(StreamingContextStates.All), quaternionSurrogate);
+
+        formatter.SurrogateSelector = selector;
+
+        return formatter;
+    }
+}
---
+++ Assets/Scripts/Save & Load System/Vector3SerializationSurrogate.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+using UnityEngine;
+
+[System.Serializable]
+public class Vector3SerializationSurrgote : ISerializationSurrogate
+{
+    public void GetObjectData(object obj, SerializationInfo info, StreamingContext context)
+    {
+        Vector3 vector = (Vector3)obj;
+        info.AddValue("x", vector.x);
+        info.AddValue("y", vector.y);
+        info.AddValue("z", vector.z);
+    }
+
+    public object SetObjectData(object obj, SerializationInfo info, StreamingContext context, ISurrogateSelector selector)
+    {
+        Vector3 vector = (Vector3)obj;
+        vector.x = (float)info.GetValue("x", typeof(float));
+        vector.y = (float)info.GetValue("y", typeof(float));
+        vector.z = (float)info.GetValue("z", typeof(float));
+        obj = vector;
+        return obj;
+    }
+}
