f976c4eed4913cd52b0841965b46c5bc7859d286
SinanEmiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2020-08-29 14:08:56 +0300

    Imported Spawn System

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 5;
+
+    private int currentHealth;
+
+    public event Action OnTookHit = delegate { };
+
+    public event Action OnDie = delegate { };
+
+    public event Action<int, int> OnHealthChanged = delegate { };
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeHit(int amount)
+    {
+        if (currentHealth <= 0)
+        {
+            return;
+        }
+
+        ModifyHealth(-amount);
+
+        if (currentHealth > 0)
+        {
+            OnTookHit();
+        }
+        else
+        {
+            OnDie();
+        }
+    }
+
+    private void ModifyHealth(int amount)
+    {
+        currentHealth += amount;
+        OnHealthChanged(currentHealth, maxHealth);
+    }
+}
---
+++ Assets/Scripts/Spawner/Spawnable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Spawnable : PooledMonoBehaviour
+{
+    [SerializeField]
+    private float returnToPoolDelay = 3f;
+
+    private void Start()
+    {
+        GetComponent<Health>().OnDie += () => ReturnToPool(returnToPoolDelay);
+    }
+}
