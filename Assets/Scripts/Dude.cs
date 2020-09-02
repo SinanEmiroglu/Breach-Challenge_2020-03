22190d4ed21509399ee0ac3630e76c2ab36ec920
SinanEmiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2020-09-02 21:33:35 +0300

    Added Tween Animation for Dude

--- Assets/Scripts/Dude.cs
+++ Assets/Scripts/Dude.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Breach AS. All rights reserved.
 
+using DG.Tweening;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -17,13 +18,21 @@ namespace Breach
         /// This is a state that needs to be saved and restored along with the saved game state
         /// </summary>
         /// 
-        int _anImportantStateValue;
+        private int _anImportantStateValue;
+        private DudeMover _mover;
+        private Collider _collider;
 
         public void SetDudeData(DudeData data)
         {
             DudeData = data;
             meshRenderer.material.color = DudeData.Color;
             _anImportantStateValue = DudeData.AnImportantStateValue;
+        }
+
+        private void Awake()
+        {
+            _mover = GetComponent<DudeMover>();
+            _collider = GetComponent<Collider>();
         }
 
         protected override void OnEnable()
@@ -36,6 +45,8 @@ namespace Breach
 
             meshRenderer.material.color = DudeData.Color;
             _anImportantStateValue = DudeData.AnImportantStateValue;
+            _collider.enabled = true;
+            _mover.enabled = true;
         }
 
         private void OnCollisionEnter(Collision collision)
@@ -49,14 +60,24 @@ namespace Breach
 
             if (collision.WasSide())
             {
+                _mover.enabled = false;
+                _collider.enabled = false;
+                transform.DOShakeScale(0.2f, 2, 20, fadeOut: false)
+                    .OnComplete(() => transform.DOScale(0, 0.25f)
+                    .OnComplete(() => ReturnToPool()));
+
                 playerHealth.TakeHit(damage);
             }
             else if (collision.WasTop())
             {
+                _mover.enabled = false;
+                _collider.enabled = false;
+                DOTween.Sequence().Append(transform.DOScaleY(0.2f, 0.35f))
+                    .Join(transform.DOScaleX(2, 0.35f))
+                    .OnComplete(() => ReturnToPool());
+
                 GameManager.Instance.BouncePlayer();
             }
-
-            ReturnToPool();
         }
 
         private Color GetRandomColorValue() => new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
